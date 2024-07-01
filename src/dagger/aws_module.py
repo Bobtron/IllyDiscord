1fad86e690cc0eedbf8a0ca1e1857ccc8e013fa2
Justin L <stalkersCanFindMeOn@linkedin.com>
2024-06-30 21:24:46 -0700

    Temporary remove boto3-stubs; using Annotated instead

--- src/dagger/aws_module.py
+++ src/dagger/aws_module.py
@@ -1,17 +1,17 @@
 from injector import Module, provider, singleton
 import boto3
-from mypy_boto3_dynamodb import DynamoDBServiceResource
+from typing import Annotated
 
 from aws.illyriad_db_dao import IllyriadDBDao
 
 class AWSModule(Module):
     @singleton
     @provider
-    def provide_illyriad_db_dao(self, dynamodb_res: DynamoDBServiceResource) -> IllyriadDBDao:
+    def provide_illyriad_db_dao(self, dynamodb_res: Annotated[object, "DynamoDBResource"]) -> IllyriadDBDao:
         return IllyriadDBDao(dynamodb_res)
 
     @singleton
     @provider
-    def provide_dynamodb_res(self) -> DynamoDBServiceResource:
+    def provide_dynamodb_res(self) -> Annotated[object, "DynamoDBResource"]:
         session = boto3.Session(profile_name='IllyriadServiceUser')
         return session.resource('dynamodb', region_name='us-west-2')
