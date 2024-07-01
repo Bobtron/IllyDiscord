44bd9d3c27a9300a5c56e6978487131839c1e723
Justin L <stalkersCanFindMeOn@linkedin.com>
2024-06-30 21:28:21 -0700

    Fix bug when no new notifications are returned

--- src/data/player_notifications.py
+++ src/data/player_notifications.py
@@ -1,9 +1,10 @@
 from typing import List
 
 from data.notification import Notification
+from data.player import Player
 
 class PlayerNotifications():
-    def __init__(self, notifications: List[Notification]):
+    def __init__(self, notifications: List[Notification], player: Player):
         self.notifications: List[Notification] = notifications
-        self.latest_notif_id: int = max(int(notif.notification_id) for notif in notifications)
         self.is_empty: bool = len(self.notifications) == 0
+        self.latest_notif_id: int = player.latest_notif_id if self.is_empty else max(int(notif.notification_id) for notif in notifications)
--- src/illyriad/illyriad.py
+++ src/illyriad/illyriad.py
@@ -5,6 +5,7 @@ from typing import List
 from illyriad.xml_parser import XMLParser
 from data.player_notifications import PlayerNotifications
 from data.notification import Notification
+from data.player import Player
 
 class Illyriad():
     @inject
@@ -12,7 +13,7 @@ class Illyriad():
         print(f'Initializing {self.__class__.__name__}')
         self.xml_parser = xml_parser
 
-    def get_player_notifications(self, player) -> PlayerNotifications:
+    def get_player_notifications(self, player: Player) -> PlayerNotifications:
         result = requests.get(f'https://elgea.illyriad.co.uk/external/notificationsapi/{player.notif_api_key}',
             params={
                 'LastNotificationID': player.latest_notif_id
@@ -24,4 +25,4 @@ class Illyriad():
 
         notifications_list: List[Notification] = self.xml_parser.parse_notifications(result.text, player.latest_notif_id)
 
-        return PlayerNotifications(notifications_list)
+        return PlayerNotifications(notifications_list, player)
